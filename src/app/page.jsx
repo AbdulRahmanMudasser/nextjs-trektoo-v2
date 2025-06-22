8ca73a975bc3bb72e2b75f486338b1fdc7bffe88
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-06-22 18:35:31 +0500

    fix: go wild section redesigned

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '@/components/layout/Navbar/Navbar';
 import HeroSection from '@/components/feature/HeroSection/HeroSection';
 import BrowseTypes from '@/components/feature/BrowseTypeSection/BrowseTypes';
-import Footer from '@/components/layout/Footer/Footer';
 import AdventureTours from '@/components/feature/AdventureSection/AdventureTours';
 import GoWildSection from '@/components/feature/GoWildSection/GoWildSection';
 import WhoWeAreSection from '@/components/feature/WhoWeAreSection/WhoWeAre';
